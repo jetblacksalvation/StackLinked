1203994e61d235969fc7283e0f40e3b469b06321
jetblacksavior <coolanimeguy666@gmail.com>
2022-11-22 16:56:43 -0700

    Added error messages

--- GraphsCPP/StackList.hpp
+++ GraphsCPP/StackList.hpp
@@ -15,7 +15,7 @@ private:
     };
     typedef StackNode<type> _StackNode;
     _StackNode* start;
-    _StackNode * end;
+    _StackNode* end;
     size_t size;
 
 public:
@@ -28,8 +28,8 @@ public:
     StackList(initialser values) {
 
         size = values.size();
-        start = new _StackNode( * (values.begin() ));
-        
+        start = new _StackNode(*(values.begin()));
+
         _StackNode* temp = start;
 
         for (int x = 1; x < size; x++) {
@@ -47,11 +47,9 @@ public:
     }
 
     void RemoveElem(size_t index) {//this function works in all cases
-        if (index > size) {
-            throw (std::out_of_range("indexing past length of stack\n"));
-            exit(-1);
-
-        }
+        CatchInvalidIndex(index);
+        CatchNullEnd();
+        CatchNullStart();
         size--;
         _StackNode* temp = start;
         for (int x = 0; x < index - 2; x++) {
@@ -65,12 +63,17 @@ public:
             PopEnd();
             return;
         }
-        _StackNode* ptr =  temp->next;
+        _StackNode* ptr = temp->next;
         temp->next = temp->next->next;
         free(ptr);
     }
     void ShiftLeft() {
         //start = end , second = start , 
+        CatchNullEnd();
+        CatchNullStart();
+        if (size < 1) {
+            return;
+        }
         _StackNode* temp = start;
         start = start->next;
         temp->next = NULL;
@@ -78,6 +81,7 @@ public:
         end = end->next;
     }
     void PrintStack() {
+
         _StackNode* temp = start;
         while (temp != NULL) {
             std::cout << temp->value << std::endl;
@@ -92,20 +96,24 @@ public:
         end = end->next;
     }
     type PopEnd() {
+        CatchNullEnd();
         size--;
         _StackNode* temp = start;//traverse until next = end;
-        while (temp->next != end) {
+        while (temp->next != end) {//get the "index" before end, free next. than return end->value 
             temp = temp->next;
         }
+        type temp1 = temp->next->value;
         free(temp->next);//free end;
         end = temp;
-        return end->value;
+        return temp1;
     }
     type& GetEnd() {
+        CatchNullEnd();
         return end->value;
     }
     //start functions
     void PushStart(type value) {
+        CatchNullStart();
         size++;
         _StackNode* temp = new _StackNode(value);
         temp->next = start;
@@ -113,7 +121,7 @@ public:
     }
     type PopStart(void) {
         //i could free start? 
-        
+        CatchNullStart();
         _StackNode* temp = start->next;
         int tem = start->value;
         free(start);
@@ -122,20 +130,43 @@ public:
         return tem;
     }
     type& GetStart(void) {
+        CatchNullStart();
         return start->value;
     }
     //indexing is working fine :P 
     type& operator[](size_t index) {
-        if (index > size) {
-            throw (std::out_of_range("indexing past length of stack\n"));
-            exit(-1);
+        CatchInvalidIndex();
+        CatchNullEnd();
+        CatchNullStart();
 
-        }
         _StackNode* temp = start;
-        for (int x = 0; x < index-1; x++) {//probably something slightly wrong with this but idgaf 
+        for (int x = 0; x < index - 1; x++) {//probably something slightly wrong with this but idgaf 
             temp = temp->next;
         }
         return temp->value;
 
+    }
+    private://catch error functions 
+    void CatchInvalidIndex(size_t index) {
+        if (index > size) {
+            while (1) {
+                throw (std::out_of_range("indexing past length of stack\n"));
+            }
+
+        }
+    }
+    void CatchNullStart(void) {
+        if (start == NULL) {
+            while (1) {
+                throw("Start/root became NULL ");
+            }
+        }
+    }
+    void CatchNullEnd(void){
+        if (end == NULL) {
+            while (1) {
+                throw("Start/root became NULL ");
+            }
+        }
     }
 };
